e9dee9de36ab9a2506bb1c3d74ff98c6e83d606f
白狐 <11125134+baihu433@user.noreply.gitee.com>
2022-11-28 14:14:54 +0000

    debug
    Signed-off-by: 白狐 <11125134+baihu433@user.noreply.gitee.com>

---
+++ ffmpeg.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+function one()
+{
+    cat <<eof
+    
+ **********************************************************************
+                        ffmpeg安装程序                 
+                      
+       1.安装ffmpeg_ARM64            2.安装ffmpeg_AMD64 
+
+       手机选择1                      电脑选择2
+
+                        0.退出安装程序
+                        
+***********************************************************************
+eof
+}
+function num()													#选项
+{
+    read -p "请输入您需要操作的项目: " number
+    case $number in
+        1)
+            ffmpeg_arm
+            ;;
+        2)
+            ffmpeg_x86_64
+            ;;
+        0)
+            exit 0
+            ;;
+
+    esac
+}
+function ffmpeg_arm()
+{
+
+        git clone https://gitee.com/baihu433/ffmpeg-arm64.git
+        mv /root/ffmpeg-arm64/ffmpeg /usr/local/bin
+        ln -s /usr/local/bin/ffmpeg /usr/bin/ffmpeg
+        chmod +x /usr/local/bin/ffmpeg
+        ffmpeg
+        echo '安装完成'
+        
+}
+function ffmpeg_x86_64()
+{
+    
+        git clone https://gitee.com/baihu433/ffmpeg-amd6.git
+        mv ~/ffmpeg-amd64/ffmpeg /usr/local/bin
+        ln -s /usr/local/bin/ffmpeg /usr/bin/ffmpeg
+        chmod +x /usr/local/bin/ffmpeg
+        ffmpeg
+        echo '安装完成'
+        
+        
+}
+function  main()
+{
+    while true
+    do
+        one
+        num
+    done
+}
+main
